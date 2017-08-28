a5439f0625cfee1281e61a7ab99cf7297c79d269
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-08-28 05:20:29 -0300

    Add Product infrastructure.

---
+++ src/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new[] { new Product() };
+        }
+    }
+}
---
+++ src/WebApi/Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebApi.Domain.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public String Description { get; set; }
+        public decimal Prive { get; set; }
+    }
+}
