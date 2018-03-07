0e4799b26381007b59b3752e47825d49007b3156
gustavodido <gustavo.di.domenico@gmail.com>
2018-03-06 22:04:24 -0800

    Update V1.3__Insert_Products.sql

--- src/WebApi/Sql_Scripts/V1.3__Insert_Products.sql
+++ src/WebApi/Sql_Scripts/V1.3__Insert_Products.sql
@@ -1,3 +1,4 @@
+-- Product examples
 INSERT INTO products (id, name, ean_code, price) values (uuid_generate_v4(), 'PHILIPS 243S7EHMB/00', '781535', 159.00);
 INSERT INTO products (id, name, ean_code, price) values (uuid_generate_v4(), 'HP PAVILION 15-CC593ND', '780842', 749.00);
 INSERT INTO products (id, name, ean_code, price) values (uuid_generate_v4(), 'APPLE IPHONE 5/5S/SE FRE CASE BLACK', '701524', 79.99);
